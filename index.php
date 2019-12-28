d6ca63513b3e59faecb4a2b7435c99112a93c5ba
vvlado23 <43526651+vvlado23@users.noreply.github.com>
2019-12-28 20:06:25 +0200

    update

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 
     <title>Uno Card Game</title>
 
-    <!-- CSS -->
-    <link href="./Css/myUno.css" type="text/css" rel="stylesheet">
+    <!-- calls -->
+    <link href="css/myUno.css" type="text/css" rel="stylesheet">
+    <script src="bootstrap/jquery-3.2.1.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <script src="js/myuno.js"></script>
   </head>
 <body style="background-color:#ff751a;">
 
@@ -19,27 +22,25 @@
 </div>
 
 <div id='board'>
+
     <div id='board_center'>
-        <img src="https://opengameart.org/sites/default/files/UNO-Back_0.png">
     </div>
 
     <div id='deck'>
-        <img src="https://opengameart.org/sites/default/files/UNO-Back_0.png">
-        <br>
-        <div style="text-align:center">
-            <button id="draw_button">Draw</button>
-            <button id="draw_button">Pass</button>
-        </div>
+        <img src="Internal/unoBack.png">        
+    </div>
+    <div class="buttons">
+            <button id='draw_button'>Draw</button>
+            <button id="pass_button">Pass</button>
     </div>
 </div>
 
 <div id='player_hand'>
-    <div class='hand'>
-        <div class='card' style="background-color: red;">8</div>
-        <div class='card' style="background-color: green;">9</div>
-        <div class='card' style="background-color: yellow;">5</div>
-        <div class='card' style="background-color: blue;">+2</div>
-        <div class='card' style="background-color: black;">+4</div>
+    
+    <div id='hand'>
+    	<?php
+    	require_once('Internal/game.php');
+    	?>
     </div>
 </div>
 
