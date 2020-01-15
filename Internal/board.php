550be4dcc65a959182ded4e783b3d095f341d53e
vvlado23 <43526651+vvlado23@users.noreply.github.com>
2020-01-15 20:34:35 +0200

    small bug fix

--- Internal/board.php
+++ Internal/board.php
@@ -5,7 +5,7 @@ function playable_card($hand_c, $down_c, $color)
 {
 	require "deck.php";
 
-	if($deck[$hand_c]->get_color()=="black")
+	if($deck[$hand_c]->get_color()=="black" || $color == "black")
 	{
 		return true;
 	}else if(($color == $deck[$hand_c]->get_color()) ||  ($deck[$down_c]->get_number() == $deck[$hand_c]->get_number()))
